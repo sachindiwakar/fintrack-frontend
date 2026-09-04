1d74c4bfa48b58af1be6920910f921f051f03e38
Sachin Diwakar <sachindiwakarp98@gmail.com>
2026-09-04 23:15:48 +0530

    define project initial structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,46 @@
+import { Navigate, Outlet, Route, Routes } from "react-router";
+import SignIn from "./pages/auth/SignIn";
+import SignUp from "./pages/auth/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Accounts from "./pages/Accounts";
+import Transactions from "./pages/Transactions";
+import Settings from "./pages/Settings";
+import useStore from "./store";
+
+const RootLayout = () => {
+  const { user } = useStore((state) => state);
+  console.log(user);
+
+  return !user ? (
+    <Navigate to="/sign-in" replace={true} />
+  ) : (
+    <>
+      {/* <Navbar/> */}
+      <div className="min-h-[cal(h-screen - 100px)]">
+        <Outlet />
+      </div>
+    </>
+  );
+};
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-2xl font-bold text-lime-600">FinTrack</h1>
-    </div>
+    <main>
+      <div className="w-full min-h-screen px-6 bg-gray-100 md:px-20 dark:bg-slate-500">
+        <Routes>
+          <Route element={<RootLayout />}>
+            <Route path="/" element={<Navigate to="/overview" replace />} />
+            <Route path="/overview" element={<Dashboard />} />
+            <Route path="/accounts" element={<Accounts />} />
+            <Route path="/transactions" element={<Transactions />} />
+            <Route path="/settings" element={<Settings />} />
+          </Route>
+
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
+      </div>
+    </main>
   );
 };
 
