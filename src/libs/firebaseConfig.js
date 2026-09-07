4df78c97cdc245dcf1d9cf9e46dd862cb064b513
Sachin Diwakar <sachindiwakarp98@gmail.com>
2026-09-07 18:06:49 +0530

    update firebase config file

--- src/libs/firebaseConfig.js
+++ src/libs/firebaseConfig.js
@@ -2,14 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC1R9K8dpX0d3l6WVwgFTjnTVuw3VH742o",
-  authDomain: "fintrack-webapp.firebaseapp.com",
-  projectId: "fintrack-webapp",
-  storageBucket: "fintrack-webapp.firebasestorage.app",
-  messagingSenderId: "789118773421",
-  appId: "1:789118773421:web:9aefab56c03a43737cbe24",
-  measurementId: "G-TRD7LQCM76",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
+
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
