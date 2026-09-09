d7c97fe9eac04b4d3b54331ea8f75dd65b60fc94
Sachin Diwakar <sachindiwakarp98@gmail.com>
2026-09-09 17:24:42 +0530

    update settings page

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import useStore from "../store";
+import Title from "../components/Title";
+import SettingForm from "../components/SettingForm";
 
 const Settings = () => {
-  return <div>Settings</div>;
+  const { user } = useStore((state) => state);
+
+  return (
+    <div className="flex flex-col items-center w-full">
+      <div className="w-full max-w-4xl px-4 py-4 my-6 shadow-lg bg-gray-50 dark:bg-black/20 md:px-10 md:my-10">
+        <div className="mb-4 border-b-2 border-gray-200 dark:border-gray-800">
+          <Title title="General Settings" />
+        </div>
+
+        <div className="py-10">
+          <p className="text-lg font-bold text-black dark:text-white">
+            Profile Information
+          </p>
+
+          <div className="flex items-center gap-4 my-8">
+            <div className="flex items-center justify-center w-12 h-12 font-bold text-2xl text-white bg-violet-600 rounded-full cursor-pointer">
+              <p>{user?.firstName?.charAt(0)}</p>
+            </div>
+
+            <p className="text-2xl font-semibold text-black dark:text-gray-400">
+              {user?.firstName}
+            </p>
+          </div>
+
+          <SettingForm />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Settings;
